35225ee3224a898d842cb13f65047ee71f804dee
Hebimalu0000 <hebimalu0000@gmail.com>
2024-10-20 00:20:40 +0000

    new

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'Hcratch-Wiki-Project-0001',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,9 @@
+const { getDataConnect, validateArgs } = require('firebase/data-connect');
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'Hcratch-Wiki-Project-0001',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
