458a3d76ae0101b02d8c7beb53275665237a7ece
Hebimalu0000 <hebimalu0000@gmail.com>
2024-10-20 03:21:51 +0000

    delete

--- dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,8 +0,0 @@
-import { getDataConnect, validateArgs } from 'firebase/data-connect';
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'Hcratch-Wiki-Project-0001',
-  location: 'us-central1'
-};
-
